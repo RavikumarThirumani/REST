890f716b52b66f760b49ac3683efda6b06a74073
Ravikumar <ravikumar.thirumani123@gmail.com>
2026-06-06 13:12:04 +0530

    db changes

--- src/main/java/com/travi/bind/User.java
+++ src/main/java/com/travi/bind/User.java
@@ -1,10 +1,14 @@
 package com.travi.bind;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class User {
 
+	@Id
 	private Integer id;
 	private String name;
 	private String email;
